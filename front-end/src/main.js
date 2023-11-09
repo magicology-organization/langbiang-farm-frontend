3b83d9e547205909d47fc602daa644a98d26c425
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-11-09 23:57:45 +0700

    added bootstrap

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+//css from bootstrap
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
